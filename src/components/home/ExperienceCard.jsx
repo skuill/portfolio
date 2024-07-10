8606c5f97e20ce5c4e27b27904a5b3d4364aa9e5
Виталий Квашин <sativkv@gmail.com>
2024-07-10 22:36:58 +0400

    moved experience card img style to scss

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -8,7 +8,7 @@ const ExperienceCard = ({ data }) => {
   return (
     <Col md="6">
       <div className="pb-5 text-center">
-        <img className="bg-white mb-3 img-fluid" width="50%" src={data.companylogo} alt="" />
+        <img className="bg-white mb-3 img-experience-card" src={data.companylogo} alt="" />
         <p className="lead">
           {data.role}
           <br />
