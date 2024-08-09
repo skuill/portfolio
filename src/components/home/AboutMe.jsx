330b47dee873a1c725d3aa5ef9c38a481dfff30a
Виталий Квашин <sativkv@gmail.com>
2024-08-09 13:57:14 +0400

    Fixed image position in AboutMe

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -35,35 +35,39 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
 
   return (
     <Jumbotron id="aboutme" className="m-0">
-      <div className="container row">
-        <div className="col-5 d-none d-lg-block align-self-center">
-          {showPic && (
-            <img
-              className="border border-secondary rounded-circle img-relative-front"
-              src={profilePicUrl}
-              alt="profilepicture"
-              width={imgSize}
-              height={imgSize}
-            />
-          )}
-        </div>
-        <div className={`col-lg-${showPic ? "7" : "12"}`}>
+      <div className="container">
+        <div className="row">
           <h2 className="display-4 mb-5 text-center">{heading}</h2>
-          <p className="lead text-center">{message}</p>
-          {resume && (
-            <p className="lead text-center">
-              <a
-                className="btn btn-outline-dark btn-lg"
-                href={resume}
-                target="_blank"
-                rel="noreferrer noopener"
-                role="button"
-                aria-label="Resume/CV"
-              >
-                Resume
-              </a>
-            </p>
-          )}
+        </div>
+        <div className="row">
+          <div className="mb-5 col-lg-5 d-block align-self-center d-flex justify-content-center text-center">
+            {showPic && (
+              <img
+                className="border border-secondary rounded-circle"
+                src={profilePicUrl}
+                alt="profilepicture"
+                width={imgSize}
+                height={imgSize}
+              />
+            )}
+          </div>
+          <div className={`col-lg-${showPic ? "7" : "12"}`}>
+            <p className="lead text-center">{message}</p>
+            {resume && (
+              <p className="lead text-center">
+                <a
+                  className="btn btn-outline-dark btn-lg"
+                  href={resume}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  role="button"
+                  aria-label="Resume/CV"
+                >
+                  Resume
+                </a>
+              </p>
+            )}
+          </div>
         </div>
       </div>
     </Jumbotron>
